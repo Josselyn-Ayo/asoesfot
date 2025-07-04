b2398f3571b5ee12c28cdf7c4b64e3b8c5490728
Josselyn-Ayo <josselyn.ayoharo@epn.edu.ec>
2025-07-04 10:59:17 -0500

    josselyn

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router";
-import { useState } from "react"
+import { useState } from "react"  //Josselyn
 
 const Header = () => {
     const [menuActive, setMenuActive] = useState(false)
